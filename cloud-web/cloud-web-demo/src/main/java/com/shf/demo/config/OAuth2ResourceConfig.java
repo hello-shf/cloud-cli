c1c0a883ffed02af729928a4457b851ef1c4e178
sunhuaifeng <877635632@qq.com>
2019-12-31 14:48:41 +0800

    oauth配置更新

---
+++ cloud-web/cloud-web-demo/src/main/java/com/shf/demo/config/OAuth2ResourceConfig.java
@@ -0,0 +1,27 @@
+/*
+package com.shf.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class OAuth2ResourceConfig {
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    //与授权服务器使用共同的密钥进行解析
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("cjs");
+        return converter;
+    }
+}
+*/
